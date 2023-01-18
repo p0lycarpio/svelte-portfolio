918e2d51424956ad77c0e69cfaa900ec77bd6443
p0lycarpio <arsene.reymond@free.fr>
2023-01-18 18:27:10 +0100

    write and update content rename parts

--- src/content/career.svelte
+++ src/content/career.svelte
@@ -12,8 +12,16 @@
     </div>
     <div class="col-md-9 col-md-push-1">
       <div class="row row-cols-md-2 mt-md-0 gx-4 mt-n2">
+        <!-- Studies -->
         <div class="order-1 order-md-0">
-          <ExperienceCard type="school" from="2021" to="aujourd'hui">
+          <ExperienceCard type="school" from="2022" to="aujourd'hui">
+            <span slot="title"
+              >Master Informatique parcours Architecte logiciel</span>
+            <span slot="description">Programmation, architecture des SI et gestion de projet.</span>
+          </ExperienceCard>
+        </div>
+        <div class="order-2 order-md-1">
+          <ExperienceCard type="school" from="2021" to="2022">
             <span slot="title"
               >LP Conception, développement et tests de logiciels – option développeur cloud</span>
             <span slot="description">Développement web, mobile, backend & cloud.</span>
@@ -26,16 +34,23 @@
               >Intégration et développement web, communication digitale, graphisme et audiovisuel.</span>
           </ExperienceCard>
         </div>
-        <div class="order-5 order-md-3">
+        <!-- <div class="order-5 order-md-3">
           <ExperienceCard type="school" from="2018" to="2019">
             <span slot="title">Baccalauréat technologique</span>
             <span slot="description">Obtention du baccalauréat STI2D. Mention assez bien.</span>
           </ExperienceCard>
+        </div> -->
+        <!-- Work experiences -->
+        <div class="order-0 order-md-0">
+          <ExperienceCard type="work" from="2022" to="aujourd'hui">
+            <span slot="title">Groupe Covéa</span>
+            <span slot="description">Ingénieur DataMLOps, en alternance.</span>
+          </ExperienceCard>
         </div>
-        <div class="order-0 order-md-1">
-          <ExperienceCard type="work" from="2021" to="aujourd'hui">
+        <div class="order-1 order-md-1">
+          <ExperienceCard type="work" from="2021" to="2022">
             <span slot="title">CGI Niort</span>
-            <span slot="description">Développeur informatique TIBCO BW, en alternance.</span>
+            <span slot="description">Développeur de webservices, en alternance.</span>
           </ExperienceCard>
         </div>
         <div class="order-2 order-md-2">
