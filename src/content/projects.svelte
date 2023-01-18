023f06fba1acf6201b3a4b33e4a264858c132879
p0lycarpio <arsene.reymond@free.fr>
2023-01-18 18:28:41 +0100

    refactor: code & UI improvements

--- src/content/projects.svelte
+++ src/content/projects.svelte
@@ -1,51 +1,45 @@
 <script>
   import ProjectCard from "../components/ProjectCard.svelte";
-  import projectsData from "../data/projects.json";
+  import allprojects from "../data/projects.json";
 
-  const filterItems = (i, d) => i.slice(0, d);
-  let root;
-  let more = 4;
+  let filtered = false;
   let current = "all";
+  let projects = allprojects.filter((prj) => prj.active == true);
+  let domains = [];
+  let visible_projects = [];
+
+  // Display only 4 elements onMount
+  let more = 4;
+  for (let i = 0; i < more; i++) {
+    visible_projects.push(projects[i]);
+  }
+
+  // Retrieve all possible domains
+  projects.forEach((element) => {
+    if (domains.indexOf(element.domain) === -1 && element.domain != undefined)
+      domains.push(element.domain);
+  });
 
   function showmore() {
-    more = more + 4;
-
-    if (more >= projectsData.length) {
-      root.querySelector("#see-more-block").style.display = "none";
-    }
-    Object.fromEntries(Object.entries(projects).slice(0, more));
+    visible_projects = visible_projects.concat(projects.slice(more, more + 4));
+    more += 4;
   }
 
   function showall() {
+    filtered = false;
+    visible_projects = projects;
     current = "all";
-    more = projectsData.length;
-    root.querySelector("#see-more-block").style.display = "none";
-
-    let elements = root.querySelectorAll("project");
-
-    let all = [...elements];
-    for (var x = 0; x < all.length; x++) {
-      all[x].parentNode.style.display = "block";
-    }
   }
 
   function filter(domain) {
-    current == domain;
     showall();
-
-    let selector = ".col-lg-6." + domain;
-    let elements = root.querySelectorAll("project");
-
-    let selections = [...elements].filter((element) => !element.closest(selector));
-    for (var x = 0; x < selections.length; x++) {
-      selections[x].parentNode.style.display = "none";
-    }
-
-    return (current = domain);
+    visible_projects = visible_projects.filter((value) => value.domain == domain);
+    filtered = true;
+    current = domain;
   }
 </script>
 
-<section class="container-xl animate fadeInUp" id="projects" bind:this={root}>
+<section class="container-xl animate fadeInUp" id="projects">
   <div class="row row-padded">
     <div class="col-md-3">
       <h2 class="section-heading">
@@ -55,35 +49,31 @@
       <div id="filtres" class="d-none d-md-block">
         <div class="filters button-group">
           <button class:selected={current === "all"} on:click={showall}>Tout</button><br />
-          <button class:selected={current === "web"} on:click={() => filter("web")}
-            >Développement</button
-          ><br />
-          <button class:selected={current === "video"} on:click={() => filter("video")}
-            >Audiovisuel</button
-          ><br />
-          <button class:selected={current === "graphisme"} on:click={() => filter("graphisme")}
-            >Graphisme</button
-          ><br />
+          {#each domains as domain}
+            <button class:selected={current === domain} on:click={() => filter(domain)}>
+              {domain.charAt(0).toUpperCase() + domain.slice(1)}</button
+            ><br />
+          {/each}
         </div>
       </div>
     </div>
 
     <div class="col-md-9 col-md-push-1">
       <div class="row elements mt-md-0 mt-n2">
-        {#each filterItems(projectsData, more) as project}
-          {#if project.active == true}
-            <ProjectCard {project} />
-          {/if}
+        {#each visible_projects as project}
+          <ProjectCard {project} />
         {/each}
       </div>
-      <div class="row justify-content-center" id="see-more-block">
-        <div class="col-6 col-md-5">
-          <div class="card d-grid text-center">
-            <button type="button" on:click={showmore} class="btn text-nowrap py-2" id="see-more"
-              >Voir plus</button>
+      {#if visible_projects.length < projects.length && !filtered}
+        <div class="row justify-content-center" id="see-more-block">
+          <div class="col-6 col-md-5">
+            <div class="card d-grid text-center">
+              <button type="button" on:click={showmore} class="btn text-nowrap py-2" id="see-more"
+                >Voir plus</button>
+            </div>
           </div>
         </div>
-      </div>
+      {/if}
     </div>
   </div>
 </section>
@@ -94,7 +84,7 @@
     &:active {
       background-color: var(--h-color) !important;
       color: var(--white-black) !important;
-      border: 0px!important;
+      border: 0px !important;
     }
   }
   .card:active {
