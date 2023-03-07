daac9d897ce585617fa146d919a690325be68acd
p0lycarpio <arsene.reymond@free.fr>
2023-03-08 00:04:27 +0100

    improve navbar icons

--- src/lib/components/ThemeSwitcher.svelte
+++ src/lib/components/ThemeSwitcher.svelte
@@ -2,6 +2,8 @@
   import { onMount } from "svelte";
   export let ref;
 
+  import IconContrast from "~icons/material-symbols/contrast-sharp"
+
   /**
    * Apply a given color theme
    * @param {string} themeName - classname to set on body
@@ -35,24 +37,21 @@
   });
 </script>
 
-<svg
-  aria-hidden="true"
-  width="24"
-  style="cursor:pointer"
-  focusable="false"
-  data-prefix="fas"
-  data-icon="adjust"
-  class="dark-mode svg-inline--fa fa-adjust fa-w-16"
-  role="img"
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 512 512"
+<span
+  class="dark-mode"
   on:click={toggleTheme}
   {ref}>
-  <path
-    d="M8 256c0 136.966 111.033 248 248 248s248-111.034 248-248S392.966 8 256 8 8 119.033 8 256zm248 184V72c101.705 0 184 82.311 184 184 0 101.705-82.311 184-184 184z" />
-</svg>
+  <IconContrast/>
+</span>
+
 
 <style>
+  span {
+    font-size: .8em;
+    position: relative;
+    top:-3px!important
+  }
+
   @media only screen and (max-width: 767px) {
     .dark-mode {
       display: none !important;
