e19c3526597221774c5c496191579f91e81a754b
p0lycarpio <arsene.reymond@free.fr>
2026-06-17 21:34:00 +0200

    feat: add paraglide

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,24 @@
 <script>
+  import { resolve } from "$app/paths";
+  import { page } from "$app/state";
+  import { locales, localizeHref } from "$lib/paraglide/runtime";
+  
   import "uno.css";
   import "@fontsource-variable/ibm-plex-sans";
   import "../lib/css/styles.scss";
+
   /**
    * @typedef {Object} Props
    * @property {import('svelte').Snippet} [children]
    */
-
   /** @type {Props} */
   let { children } = $props();
 </script>
 
 {@render children?.()}
+
+<div style="display:none">
+  {#each locales as locale (locale)}
+    <a href={resolve(localizeHref(page.url.pathname, { locale }))}>{locale}</a>
+  {/each}
+</div>
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,18 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { imagetools } from 'vite-imagetools'
-import Icons from 'unplugin-icons/vite'
-import UnoCSS from 'unocss/vite'
+import { imagetools } from 'vite-imagetools';
+import Icons from 'unplugin-icons/vite';
+import UnoCSS from 'unocss/vite';
 
 const config = {
     plugins: [
         UnoCSS(),
         sveltekit(),
         imagetools(),
-        Icons({
-            compiler: 'svelte',
-        }),
+        Icons({ compiler: 'svelte' }),
+        paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/paraglide' })
     ],
-    define: {
-        __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
-    },
+    define: { __BUILD_DATE__: JSON.stringify(new Date().toISOString()) }
 };
 
 export default config;
